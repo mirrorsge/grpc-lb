9249641341ba31b384f0292d21c9b0ac167dd5f7
jianjunli <ljj.sanl@gmail.com>
2021-01-26 14:23:50 +0800

    调用adapter

--- examples/consul/client/main.go
+++ examples/consul/client/main.go
@@ -2,27 +2,14 @@ package main
 
 import (
 	"context"
-	consulapi "github.com/hashicorp/consul/api"
-	"github.com/mirrorsge/grpc-lb/balancer"
 	"github.com/mirrorsge/grpc-lb/examples/proto"
-	"github.com/mirrorsge/grpc-lb/registry/consul"
-	"google.golang.org/grpc"
 	"log"
 	"time"
 )
 
 func main() {
-	consul.RegisterResolver("consul", &consulapi.Config{Address: "http://59.110.162.134:8500"}, "alpha:1.0")
-	conn, err := grpc.Dial("consul:///", grpc.WithInsecure(), grpc.WithBalancerName(balancer.RoundRobin))
-	if err != nil {
-		log.Printf("grpc dial: %s", err)
-		return
-	}
-	defer conn.Close()
-
-	client := proto.NewAlphaClient(conn)
-
-	res, err := client.Hello(context.Background(), &proto.HelloReq{
+	ctx := context.Background()
+	res, err := proto.AlphaAdapter.Hello(ctx, &proto.HelloReq{
 		Name: "lijianjun",
 		Time: time.Now().String(),
 	})
